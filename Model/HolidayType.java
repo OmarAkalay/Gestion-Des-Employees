7cf9f67a57f3eee5508067a1fc42c53618844589
OmarAkalay <akalayomar28@gmail.com>
2024-12-26 15:33:50 +0100

    Delete Model directory

--- Model/HolidayType.java
+++
@@ -1,7 +0,0 @@
-package Model;
-
-public enum HolidayType {
-    CONGE_PAYE,
-    CONGE_NON_PAYE,
-    CONGE_MALADIE
-}
--- Model/Poste.java
+++
@@ -1,7 +0,0 @@
-package Model;
-
-public enum Poste {
-    INGENIEUR_ETUDE_ET_DEVELOPPEMENT,
-    TEAM_LEADER,
-    PILOTE
-}
